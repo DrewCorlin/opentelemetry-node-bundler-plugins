491b5e3ef2834c8023383d37c929f35f4c956990
Abhi Agarwal <abhiaagarwal01@gmail.com>
2026-06-30 22:14:49 -0400

    remove tslib, ignore tsdown config from linting
    Signed-off-by: Abhi Agarwal <abhiaagarwal01@gmail.com>

--- packages/opentelemetry-rollup-plugin-node/src/plugin.ts
+++ packages/opentelemetry-rollup-plugin-node/src/plugin.ts
@@ -138,7 +138,8 @@ export function openTelemetryPlugin(
       const config = meta.instrumentationName
         ? otelPackageToInstrumentationConfig[meta.instrumentationName]
         : undefined;
-      if (!config) return null;
+      const moduleVersion = meta.moduleVersion;
+      if (!config || !moduleVersion) return null;
 
       const packageConfig = getPackageConfig({
         pluginConfig,
@@ -147,7 +148,7 @@ export function openTelemetryPlugin(
 
       return wrapModule(code, {
         path: join(meta.extractedModule.package, meta.extractedModule.path),
-        moduleVersion: meta.moduleVersion!,
+        moduleVersion,
         instrumentationName: meta.instrumentationName,
         oTelInstrumentationClass: config.oTelInstrumentationClass,
         oTelInstrumentationPackage: config.oTelInstrumentationPackage,
